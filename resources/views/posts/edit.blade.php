aea5a7550f0aac6cab65d0bd7eb1abb9e2429b19
suzume715 <suzume2eg@gmail.com>
2024-02-24 02:48:51 +0900

    ブログ投稿の編集 ブログ投稿編集画面の保存時入力チェック処理実装

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,13 +9,16 @@
     <div class="content">
         <form action="/posts/{{ $post->id }}" method="POST">
             @csrf
+            @method('PUT')
             <div class='content__title'>
                 <h2>タイトル</h2>
                 <input type='text' name='post[title]' value="{{ $post->title }}">
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class='content__body'>
                 <h2>本文</h2>
                 <input type='text' name='post[body]' value="{{ $post->body }}">
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="保存">
         </form>
