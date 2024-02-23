f9a6a1ee9dd7d01134171520928511554d85db1d
suzume715 <suzume2eg@gmail.com>
2024-02-24 02:29:55 +0900

    ブログ投稿の編集 ブログ投稿編集画面の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,4 +28,9 @@ class PostController extends Controller
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
+    public function edit(Post $post)
+    {
+    return view('posts.edit')->with(['post' => $post]);
+    }
 }
